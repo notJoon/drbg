a6e0af818e93603de024893a8405c14bdac55677
Lee ByeongJun <lbj199874@gmail.com>
2024-05-07 12:51:40 +0900

    refactor

--- main.go
+++ main.go
@@ -21,29 +21,29 @@ func main() {
 	rank := flag.Bool("rank", false, "Run Binary Matrix Rank Test")
 	dft := flag.Bool("dft", false, "Run Discrete Fourier Transform (Spectral) Test")
 
-	nonOverlappingTemplate := flag.Bool("non-overlapping", false, "Run Non-overlapping Template Matching Test")
-	overlappingTemplate := flag.Bool("overlapping", false, "Run Overlapping Template Matching Test")
+	nonOverlappingTemplate := flag.Bool("non-overlapping", false, "Run Non-overlapping Template Matching Test.\nDefault template is \"000000001\" and block size is 10 bits.")
+	overlappingTemplate := flag.Bool("overlapping", false, "Run Overlapping Template Matching Test.\nDefault template is \"000000001\" and block size is 10 bits.")
 	// specifies the template B to match. Must be string of ones and zeros (e.g. "001")
-	templateB := flag.String("template", "", "The template B to be matched (a string of ones and zeros)")
+	templateB := flag.String("template", "000000001", "The template B to be matched (a string of ones and zeros)")
 	// specified the length of the substrting to test, in bits.
-	blockSize := flag.Uint64("block-size", 0, "The length in bits of the substring to be tested")
+	blockSize := flag.Uint64("block-size", 20, "The length in bits of the substring to be tested")
 
 	universal := flag.Bool("universal", false, "Run Maurer's Universal Statistical Test")
 
-	linearComplexity := flag.Bool("linear", false, "Run Linear Complexity Test")
-	inputSize := flag.Uint64("m", 0, "The length of the block to be tested")
+	linearComplexity := flag.Bool("linear", false, "Run Linear Complexity Test. Default block size is 500 bits.")
+	inputSize := flag.Uint64("m", 500, "The length of the block to be tested")
 
 	serial := flag.Bool("serial", false, "Run Serial Test")
 	approximateEntropy := flag.Bool("entropy", false, "Run Approximate Entropy Test")
 
-	cusum := flag.Bool("cusum", false, "Run Cumulative Sums (Cusums) Test")
+	cusum := flag.Bool("cusum", false, "Run Cumulative Sums (Cusums) Test. Default mode is 0 (forward).")
 	mode := flag.Int("mode", 0, "The mode of the test (0 or 1)")
 
-	// randomExcursions := flag.Bool("random-excursions", false, "Run Random Excursions Test")
-	// randomExcursionsVariant := flag.Bool("random-excursions-variant", false, "Run Random Excursions Variant Test")
+	randomExcursions := flag.Bool("random-excursions", false, "Run Random Excursions Test")
+	randomExcursionsVariant := flag.Bool("random-excursions-variant", false, "Run Random Excursions Variant Test")
 
 	filename := flag.String("file", "", "File containing the random bits")
-	verbose := flag.Bool("verbose", false, "Enable verbose output")
+
 	help := flag.Bool("help", false, "Show help message")
 	flag.Parse()
 
@@ -84,101 +84,72 @@ func main() {
 	t.AppendHeader(table.Row{"NIST Statistical Test Suite", "p-value", "Result"})
 
 	if *allTests || *frequency {
+		testName := "Frequency (Monobit) Test"
 		p_val, isRandom, err := nist.FrequencyTest(bs)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Frequency (Monobit) Test", p_val, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Frequency (Monobit) Test", p_val, "Fail"})
-		}
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
 	}
 	if *allTests || *blockFrequency {
+		testName := "Frequency Test within a Block"
 		p_val, isRandom, err := nist.BlockFrequencyTest(bs, uint64(100))
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Frequency Test within a Block", p_val, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Frequency Test within a Block", p_val, "Fail"})
-		}
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *runs {
+		testName := "Runs Test"
 		p_val, isRandom, err := nist.Runs(bs)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Runs Test", p_val, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Runs Test", p_val, "Fail"})
-		}
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *longestRun {
+		testName := "Test for the Longest Run of Ones in a Block"
 		p_val, isRandom, err := nist.LongestRunOfOnes(bs)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Test for the Longest Run of Ones in a Block", p_val, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Test for the Longest Run of Ones in a Block", p_val, "Fail"})
-		}
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *rank {
+		testName := "Binary Matrix Rank Test"
 		p_val, isRandom, err := nist.Rank(bs)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Binary Matrix Rank Test", p_val, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Binary Matrix Rank Test", p_val, "Fail"})
-		}
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *dft {
+		testName := "Discrete Fourier Transform (Spectral) Test"
 		p_val, isRandom, err := nist.DFT(bs)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Discrete Fourier Transform (Spectral) Test", p_val, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Discrete Fourier Transform (Spectral) Test", p_val, "Fail"})
-		}
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *nonOverlappingTemplate {
+		testName := "Non-overlapping Template Matching Test"
 		if *templateB == "" {
 			fmt.Println("Error (non-overlapping template test): template B is required for Non-overlapping Template Matching Test.\nUse -template \"001\" (or other tmeplate)")
 			os.Exit(1)
@@ -205,16 +176,11 @@ func main() {
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Non-overlapping Template Matching Test", p_value, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Non-overlapping Template Matching Test", p_value, "Fail"})
-		}
+		writeResult(t, testName, p_value, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *overlappingTemplate {
+		testName := "Overlapping Template Matching Test"
 		if *templateB == "" {
 			fmt.Println("Error (overlapping templelate test): template B is required for Non-overlapping Template Matching Test.\nUse -template \"001\" (or other tmeplate)")
 			os.Exit(1)
@@ -241,32 +207,22 @@ func main() {
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Overlapping Template Matching Test", p_value, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Overlapping Template Matching Test", p_value, "Fail"})
-		}
+		writeResult(t, testName, p_value, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *universal {
+		testName := "Maurer's Universal Statistical Test"
 		p_val, isRandom, err := nist.UniversalRecommendedValues(bs)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Maurer's Universal Statistical Test", p_val, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Maurer's Universal Statistical Test", p_val, "Fail"})
-		}
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *linearComplexity {
+		testName := "Linear Complexity Test"
 		if *inputSize < 500 || *inputSize > 5000 {
 			fmt.Println("Error: input size must be between 500 and 5000")
 			os.Exit(1)
@@ -277,16 +233,11 @@ func main() {
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Linear Complexity Test", p_val, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Linear Complexity Test", p_val, "Fail"})
-		}
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *serial {
+		testName := "Serial Test"
 		p_val, isRandom, err := nist.Serial(10, bs)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
@@ -295,51 +246,82 @@ func main() {
 
 		if isRandom != nil {
 			pass++
-			t.AppendRow([]interface{}{"Serial Test", p_val, "Pass"})
+			t.AppendRow([]interface{}{testName, fmt.Sprintf("%.2f", p_val), "Pass"})
 		} else {
 			fail++
-			t.AppendRow([]interface{}{"Serial Test", p_val, "Fail"})
+			t.AppendRow([]interface{}{testName, fmt.Sprintf("%.2f", p_val), "Fail"})
 		}
 	}
 
 	if *allTests || *approximateEntropy {
+		testName := "Approximate Entropy Test"
 		p_val, isRandom, err := nist.ApproximateEntropy(15, bs)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
 
-		if isRandom {
-			pass++
-			t.AppendRow([]interface{}{"Approximate Entropy Test", p_val, "Pass"})
-		} else {
-			fail++
-			t.AppendRow([]interface{}{"Approximate Entropy Test", p_val, "Fail"})
-		}
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
 	}
 
 	if *allTests || *cusum {
+		testName := "Cumulative Sums Test"
 		p_val, isRandom, err := nist.CumulativeSums(*mode, bs)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 		}
 
-		if isRandom {
+		writeResult(t, testName, p_val, isRandom, &pass, &fail)
+	}
+
+	if *allTests || *randomExcursions {
+		testName := "Random Excursions Test"
+		p_val, isRandom, err := nist.RandomExcursions(bs)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+
+		if isRandom != nil {
 			pass++
-			t.AppendRow([]interface{}{"Approximate Entropy Test", p_val, "Pass"})
+			t.AppendRow([]interface{}{testName, fmt.Sprintf("%.2f", p_val), "Pass"})
 		} else {
 			fail++
-			t.AppendRow([]interface{}{"Approximate Entropy Test", p_val, "Fail"})
+			t.AppendRow([]interface{}{testName, fmt.Sprintf("%.2f", p_val), "Fail"})
 		}
 	}
 
-	if *verbose {
-		fmt.Println("Verbose output enabled")
-		// TODO: detailed output
+	if *allTests || *randomExcursionsVariant {
+		testName := "Random Excursions Variant Test"
+		p_val, isRandom, err := nist.RandomExcursionsVariant(bs)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+
+		if isRandom != nil {
+			pass++
+			t.AppendRow([]interface{}{testName, fmt.Sprintf("%.2f", p_val), "Pass"})
+		} else {
+			fail++
+			t.AppendRow([]interface{}{testName, fmt.Sprintf("%.2f", p_val), "Fail"})
+		}
 	}
 
 	t.AppendFooter(table.Row{"", "Total Tests", pass + fail})
 	t.AppendFooter(table.Row{"", "Pass", pass})
 	t.AppendFooter(table.Row{"", "Fail", fail})
 	t.Render()
+}
+
+// writeResult writes the result of a test to the table
+func writeResult(t table.Writer, testName string, pValue float64, isRandom bool, pass *int, fail *int) {
+	result := "Fail"
+	if isRandom {
+		result = "Pass"
+		*pass += 1
+	} else {
+		*fail += 1
+	}
+	t.AppendRow([]interface{}{testName, fmt.Sprintf("%.2f", pValue), result})
 }
